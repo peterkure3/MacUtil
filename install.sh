6c8ced8fe5bf7fc962afc8a4b26a27d1f5834ef5
Peter Kure <peterkure256@gmail.com>
2023-12-09 13:00:46 +0300

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo "███╗░░░███╗░█████╗░░█████╗░██╗░░░██╗████████╗██╗██╗░░░░░  ██╗░░░██╗░░███╗░░░░░░█████╗░░░░░█████╗░"
-echo "████╗░████║██╔══██╗██╔══██╗██║░░░██║╚══██╔══╝██║██║░░░░░  ██║░░░██║░████║░░░░░██╔══██╗░░░██╔══██╗"
-echo "██╔████╔██║███████║██║░░╚═╝██║░░░██║░░░██║░░░██║██║░░░░░  ╚██╗░██╔╝██╔██║░░░░░██║░░██║░░░██║░░██║"
-echo "██║╚██╔╝██║██╔══██║██║░░██╗██║░░░██║░░░██║░░░██║██║░░░░░  ░╚████╔╝░╚═╝██║░░░░░██║░░██║░░░██║░░██║"
-echo "██║░╚═╝░██║██║░░██║╚█████╔╝╚██████╔╝░░░██║░░░██║███████╗  ░░╚██╔╝░░███████╗██╗╚█████╔╝██╗╚█████╔╝"
-echo "╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░░░░╚═╝░░░╚═╝╚══════╝  ░░░╚═╝░░░╚══════╝╚═╝░╚════╝░╚═╝░╚════╝░"
+# echo "███╗░░░███╗░█████╗░░█████╗░██╗░░░██╗████████╗██╗██╗░░░░░  ██╗░░░██╗░░███╗░░░░░░█████╗░░░░░█████╗░"
+# echo "████╗░████║██╔══██╗██╔══██╗██║░░░██║╚══██╔══╝██║██║░░░░░  ██║░░░██║░████║░░░░░██╔══██╗░░░██╔══██╗"
+# echo "██╔████╔██║███████║██║░░╚═╝██║░░░██║░░░██║░░░██║██║░░░░░  ╚██╗░██╔╝██╔██║░░░░░██║░░██║░░░██║░░██║"
+# echo "██║╚██╔╝██║██╔══██║██║░░██╗██║░░░██║░░░██║░░░██║██║░░░░░  ░╚████╔╝░╚═╝██║░░░░░██║░░██║░░░██║░░██║"
+# echo "██║░╚═╝░██║██║░░██║╚█████╔╝╚██████╔╝░░░██║░░░██║███████╗  ░░╚██╔╝░░███████╗██╗╚█████╔╝██╗╚█████╔╝"
+# echo "╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░░░░╚═╝░░░╚═╝╚══════╝  ░░░╚═╝░░░╚══════╝╚═╝░╚════╝░╚═╝░╚════╝░"
 
 echo "Press ENTER to continue."
 read -p ""
 echo "If you don't have Homebrew installed, please press ENTER to install it."
 echo -p ""
+
+# Read applications from JSON file
+read_applications_from_json() {
+    if [ -f "applications.json" ]; then
+        mapfile -t applications < <(jq -r '.applications[]' applications.json)
+        options+=("${applications[@]}")
+    else
+        echo "Warning: applications.json not found."
+    fi
+}
+
 # Function to check if Homebrew is installed
 check_homebrew() {
     if ! command -v brew >/dev/null 2>&1; then
@@ -22,62 +33,80 @@ check_homebrew() {
 }
 
 # Function to show a popup notification
-# show_notification() {
-#     osascript -e 'display notification "All selected applications have been installed." with title "Installation Complete"'
-# }
+show_notification() {
+    osascript -e 'display notification "All selected applications have been installed." with title "Installation Complete"'
+}
 
 # Function to install a package
 install_package() {
-    echo "Installing $1..."
-    brew install $1
+    local app_name="$1"
+    echo "Installing $app_name..."
+    brew install "$app_name" | tee /dev/tty
 }
 
 # Check for Homebrew installation
 check_homebrew
 
-echo "Select applications to install (press ENTER after your selections):"
+# Read applications from JSON file
+read_applications_from_json
+
+# Display checkboxes for application selection
+show_checkbox_menu() {
+    local menu_options=()
+    for app in "${options[@]}"; do
+        menu_options+=("$app" "off")
+    done
+
+    selected_apps=$(whiptail --title "Select applications to install" --checklist \
+        "Use spacebar to select applications:" 20 60 10 "${menu_options[@]}" 3>&1 1>&2 2>&3)
+
+    if [ $? -eq 0 ]; then
+        selected_apps=($selected_apps)
+    else
+        echo "Selection canceled. Exiting."
+        exit 0
+    fi
+}
 
 # Function to check if an application is already installed
 is_installed() {
-    if type "$1" > /dev/null 2>&1; then
-        echo "$1 is already installed."
+    local app_name="$1"
+    if type "$app_name" > /dev/null 2>&1; then
+        echo "$app_name is already installed."
         return 0 # Return 0 indicates the app is installed
     else
         return 1 # Return 1 indicates the app is not installed
     fi
 }
 
-# Function to install a package
-install_package() {
-    if ! is_installed "$1"; then
-        echo "Installing $1..."
-        brew install $1 | tee /dev/tty
-    else
-        echo "Skipping installation of $1 as it is already installed."
-    fi
+# Function to install selected applications
+install_selected_apps() {
+    for selected_app in "${selected_apps[@]}"; do
+        install_package "$selected_app"
+    done
 }
 
 # Function to update Homebrew
 update_homebrew() {
     echo "Updating Homebrew..."
-    brew update
+    brew update | pv -l | tee /dev/tty
 }
 
 # Function to upgrade all installed packages
 upgrade_packages() {
     echo "Upgrading all installed packages..."
-    brew upgrade
+    brew upgrade | pv -l | tee /dev/tty
 }
-
-# Define the options
-options=("update" "upgrade" "git" "node" "python" "firefox" "vlc" "done")
 
 # Generate the menu
 PS3='Please enter your choice: '
+options+=("custom" "done") # Add "custom" option
 select opt in "${options[@]}"
 do
     if [ "$opt" == "done" ]; then
         break
+    elif [ "$opt" == "custom" ]; then
+        custom_installation
     elif [[ " ${options[@]} " =~ " ${opt} " ]]; then
         install_package "$opt"
     elif [ "$opt" == "update" ]; then
@@ -88,10 +117,13 @@ do
         echo "Invalid option $REPLY"
     fi
 done
+
+# List successfully installed applications after installation completes
+list_installed_apps
+
 # Exiting
 echo "Exiting MacUtil..."
 sleep 3
 echo "Done."
-# # After the installation process
-# echo "All applications have been installed."
-# show_notification
+# After the installation process
+show_notification
